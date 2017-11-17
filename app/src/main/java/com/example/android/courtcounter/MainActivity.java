b5392d410644809b2fa332bb858d4695cbc0ac10
Capri84 <capri84@mail.ru>
2017-11-17 14:32:12 +0300

    CounterApp finished.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +27,67 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the "+3 points" button for Team A is clicked.
      */
-    public void plusThreeButton(View view) {
+    public void plusThreeButtonA(View view) {
         scoreTeamA = scoreTeamA + 3;
-        display(scoreTeamA);
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * This method is called when the "+2 points" button for Team A is clicked.
      */
-    public void plusTwoButton(View view) {
+    public void plusTwoButtonA(View view) {
         scoreTeamA = scoreTeamA + 2;
-        display(scoreTeamA);
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * This method is called when the "Free throw" button for Team A is clicked.
      */
-    public void freeThrow(View view) {
+    public void freeThrowA(View view) {
         scoreTeamA = scoreTeamA + 1;
-        display(scoreTeamA);
+        displayForTeamA(scoreTeamA);
     }
 
     /**
-     * This method displays the given quantity value on the screen.
+     * Displays the given score for Team B.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.team_a_score);
-        quantityTextView.setText("" + number);
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method is called when the "+3 points" button for Team B is clicked.
+     */
+    public void plusThreeButtonB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the "+2 points" button for Team B is clicked.
+     */
+    public void plusTwoButtonB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the "Free throw" button for Team B is clicked.
+     */
+    public void freeThrowB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the "Reset" button is clicked.
+     * It resets the score for both teams back to 0.
+     */
+    public void reset(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
